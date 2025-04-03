7d82cbf82cc84ae2c5f2dc70f3bebf5b3d3a5b97
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-04-03 14:42:35 +0200

    just import asyncio_loop

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,1 @@
-import asyncio
-
-import pytest
-from panel.tests.conftest import server_cleanup  # noqa: F401
-
-
-@pytest.fixture
-def asyncio_loop():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(asyncio.new_event_loop())
-    yield
-    loop.stop()
-    loop.close()
+from panel.tests.conftest import asyncio_loop, server_cleanup  # noqa: F401
